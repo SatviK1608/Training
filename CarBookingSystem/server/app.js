098b82324ecf72465d700608614de4c63a110aae
SatviK1608 <satvik1608@gmail.com>
2024-07-01 16:42:11 +0530

    basic changes

--- CarBookingSystem/server/app.js
+++ CarBookingSystem/server/app.js
@@ -16,7 +16,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/rides',authenticateToken, rideRoutes);
 
 sequelize.sync().then(() => {
-  app.listen(process.env.PORT || 3000, () => {
+  app.listen(process.env.PORT || 5000, () => {
     console.log(`Server running on port ${process.env.PORT || 3000}`);
   });
 }).catch((error) => {
